229c2ae555ef6a523dd66dc4080a62285327e087
samiralajmovic <alajmovic.samir@gmail.com>
2020-12-25 11:40:21 +0100

    Add loading screen on app initialization

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@ import { App } from 'components/app/app.js';
 import { Store } from 'state/store.js';
 
 window.addEventListener('DOMContentLoaded', () => {
+  setLoading();
   main();
 });
+
+function setLoading() {
+  document.body.innerHTML = `
+    <div class="app-loader">
+      <h1>Loading Pinto</h1>
+      <div class="fa-4x">
+        <i class="fas fa-stroopwafel fa-spin"></i>
+      </div>
+    </div>
+  `;
+}
 
 function main() {
   init();
 
   async function init() {
     const { element, onRenderComplete } = App(await Store);
-
     render(document.body, element);
 
     onRenderComplete();
